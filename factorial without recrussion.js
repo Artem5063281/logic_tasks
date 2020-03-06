a490f459c57383183a4e94833b7e58c92a1c75c1
Artem5063281 <5063281@gmail.com>
2020-03-06 09:19:27 +0200

    recrussion

---
+++ factorial without recrussion.js
@@ -0,0 +1,9 @@
+function factorial(x){
+    if (x===1||x===0) return 1
+    else {
+        for (var n=x-1; n>0; n--){
+            x=x*n;
+        }
+        return x;
+    }
+    }
---
+++ task_2.js
@@ -0,0 +1,7 @@
+function compare(arr1, arr2) {
+    if (arr1.length != arr2.length) return false
+    else {
+        for (var i = 0; i < arr1.length; i++)
+            if (arr1[i] === arr2[i]) return true
+    }
+}
