1ea649967f873704cc488b6940d519de72a2dc2f
Artem5063281 <5063281@gmail.com>
2020-03-13 10:11:56 +0200

    compare

--- task_2.js
+++ task_2.js
@@ -1,7 +1,6 @@
 function compare(arr1, arr2) {
-    if (arr1.length != arr2.length) return false
-    else {
+    if (arr1.length === arr2.length) {
         for (var i = 0; i < arr1.length; i++)
             if (arr1[i] === arr2[i]) return true
-    }
+    } else return false
 }
