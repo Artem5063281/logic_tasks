b7092f92c04c9754809ac19a04912414c8e48cf6
Artem5063281 <5063281@gmail.com>
2020-04-10 18:25:24 +0300

    task_2 without if-else

--- task_2.js
+++ task_2.js
@@ -1,8 +1,10 @@
-function compare(arr1, arr2) {
-    if (arr1.length === arr2.length) {
-        for (var value of arr1)
-            if (arr1[value] === arr2[value]) return true
-            else return false
+    function compare(arr1, arr2) {
+        if (arr1.length === arr2.length) {
+            for (var value of arr1) {
+                (arr1[value] === arr2[value])
+            }
+            return true
+
+        }
+        return false
     }
-    return false
-}
